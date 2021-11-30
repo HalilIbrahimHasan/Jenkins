07a50960d2d9a4bb64edbb91d96321f8ab37f624
Ibrahim Kalin <hisetkal@gmail.com>
2021-11-29 23:37:09 -0500

    final

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -3,6 +3,8 @@ package com.qascript;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -26,8 +28,7 @@ public class FirstTest {
         System.out.println("Title of the page is: " + driver.getTitle());
         Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
         driver.quit();
-        driver.get("https://www.amazon.com/");
-        System.out.println(driver.getTitle());
+        driver.findElement(By.xpath("//*[@class='gLFyf gsfi']")).sendKeys("phone"+ Keys.ENTER);
 
     }
 
